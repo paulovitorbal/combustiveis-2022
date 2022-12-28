6a921c1bce6567816edf0580fb1ac13f5c7d58b4
F8202255 Paulo Vitor Bettini de Paiva Lima <bettini@bb.com.br>
2022-12-28 16:44:32 -0300

    2022 commit

--- combustiveis.php
+++ combustiveis.php
@@ -19,14 +19,19 @@ function formatPtBrNumber(string $number):string{
 $motolog = fopen(getFileName('motolog_csv_dobló_2022-12-27_192358.csv'), 'r');
 $headers = fgetcsv($motolog);
 $fuelConsumptions = [];
+$start = DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2022-03-19 00:00:00');
 while(($fields = fgetcsv($motolog)) !== false){
     $date = DateTimeImmutable::createFromFormat('Y-m-d H:i:s', $fields[0]);
     if ($date === false){
         throw new RuntimeException('something wrong happened: [' . implode(', ', $fields) . ']' );
     }
+    if ($date < $start){
+        continue;
+    }
     $fuelConsumptions[$date->format('Y-m-d')] = [
         'date' => $date->format('Y-m-d'),
         'week' => (int) $date->format('oW'),
+        'type' => $fields[11],
         'quantity' => $fields[12],
         'price' => $fields[14],
     ];
@@ -39,14 +44,17 @@ $precos = [];
 $headers = fgetcsv($precosANP, null, ';');//skipping headers
 
 while(($fields = fgetcsv($precosANP, null, ';')) !== false){
-    if ($fields[5] !== 'GASOLINA COMUM'){
-        continue;
+
+    if ($fields[5] === 'GASOLINA COMUM'){
+        $type = 'pb';
+    } else {
+        $type = 'ethanol';
     }
     $date = DateTimeImmutable::createFromFormat('d/m/Y H:i:s', $fields[0] . ' 00:00:00');
     if ($date === false){
         throw new RuntimeException('something wrong happened: [' . implode(', ', $fields) . ']' );
     }
-    $precos[(int) $date->format('oW')] = [
+    $precos[$type][(int) $date->format('oW')] = [
         'date' => $date->format('Y-m-d'),
         'week' => (int) $date->format('oW'),
         'price' => [
@@ -57,7 +65,8 @@ while(($fields = fgetcsv($precosANP, null, ';')) !== false){
         ]
     ];
 }
-ksort($precos);
+ksort($precos['ethanol']);
+ksort($precos['pb']);
 bcscale(3);
 $results = [];
 $cumulativo = [
@@ -76,23 +85,24 @@ function getWeek(int $week, array $precos): int
 
 foreach($fuelConsumptions as $fuelConsumption){
 
-    $week = getWeek((int) $fuelConsumption['week'], $precos);
+    $week = getWeek((int) $fuelConsumption['week'], $precos[$fuelConsumption['type']]);
     $temp = $fuelConsumption;
     $totalAbastecimento = bcmul($fuelConsumption['quantity'], $fuelConsumption['price']);
+    $temp['type'] = $fuelConsumption['type'];
     $temp['quantity'] = formatBr($temp['quantity']);
     $temp['price'] = formatBr($temp['price']);
     $temp['totalAbastecimento'] = formatBr($totalAbastecimento);
     $temp['precoSemanal'] = '----';
-    $temp['precoMedio'] = formatBr($precos[$week]['price']['avg']);
-    $temp['precoMinimo'] = formatBr($precos[$week]['price']['min']);
-    $temp['precoMaximo'] = formatBr($precos[$week]['price']['max']);
-    $temp['precoDesvioPadrao'] = formatBr($precos[$week]['price']['sd']);
+    $temp['precoMedio'] = formatBr($precos[$fuelConsumption['type']][$week]['price']['avg']);
+    $temp['precoMinimo'] = formatBr($precos[$fuelConsumption['type']][$week]['price']['min']);
+    $temp['precoMaximo'] = formatBr($precos[$fuelConsumption['type']][$week]['price']['max']);
+    $temp['precoDesvioPadrao'] = formatBr($precos[$fuelConsumption['type']][$week]['price']['sd']);
     $temp['precoForaClube'] = '----';
-    $foraClube['avg'] = bcmul($fuelConsumption['quantity'], $precos[$week]['price']['avg']);
+    $foraClube['avg'] = bcmul($fuelConsumption['quantity'], $precos[$fuelConsumption['type']][$week]['price']['avg']);
     $temp['foraClubeUsandoMedio'] = formatBr($foraClube['avg']);
-    $foraClube['min'] = bcmul($fuelConsumption['quantity'], $precos[$week]['price']['min']);
+    $foraClube['min'] = bcmul($fuelConsumption['quantity'], $precos[$fuelConsumption['type']][$week]['price']['min']);
     $temp['foraClubeUsandoMinimo'] = formatBr($foraClube['min']);
-    $foraClube['max'] = bcmul($fuelConsumption['quantity'], $precos[$week]['price']['max']);
+    $foraClube['max'] = bcmul($fuelConsumption['quantity'], $precos[$fuelConsumption['type']][$week]['price']['max']);
     $temp['foraClubeUsandoMaximo'] = formatBr($foraClube['max']);
     $temp['totaisCumulativos'] = '----';
     $cumulativo['base'] = (bcadd($cumulativo['base'], $totalAbastecimento));
